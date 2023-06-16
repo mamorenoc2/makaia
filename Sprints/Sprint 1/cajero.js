693987c729fe03cf628e5e965e8e56e351b18843
Manuela Moreno <49201450+mamorenoc2@users.noreply.github.com>
2023-06-16 14:45:17 -0500

    Ya me carga los billeticos!

--- Sprints/Sprint 1/cajero.js
+++ Sprints/Sprint 1/cajero.js
@@ -9,7 +9,7 @@ const cajero = [
     { billetes: 10000, cantidad: 0 },
     { billetes: 20000, cantidad: 0 },
     { billetes: 50000, cantidad: 0 },
-    { billetes: 100000, cantidad: 0 },
+    { billetes: 100000, cantidad: 0 }
 ];
 
 let usuarioActual = null;
@@ -37,6 +37,7 @@ function inicioSesion() {
 
 function mostrarCajero() {
     if (usuarioActual.tipo === 1) {
+        //Solo los administradores pueden cargar dinero
         cargarCajero();
     } else {
         retirarDinero();
@@ -44,16 +45,65 @@ function mostrarCajero() {
 }
 
 function cargarCajero() {
+    let billeteDe5 = parseInt(prompt("Ingrese la cantidad de billetes de $5000:"));
+    let billeteDe10 = parseInt(prompt("Ingrese la cantidad de billetes de $10000:"));
+    let billeteDe20 = parseInt(prompt("Ingrese la cantidad de billetes de $20000:"));
+    let billeteDe50 = parseInt(prompt("Ingrese la cantidad de billetes de $50000:"));
+    let billeteDe100 = parseInt(prompt("Ingrese la cantidad de billetes de $100000:"));
+
+    cajero[0].cantidad += billeteDe5;
+    cajero[1].cantidad += billeteDe10;
+    cajero[2].cantidad += billeteDe20;
+    cajero[3].cantidad += billeteDe50;
+    cajero[4].cantidad += billeteDe100;
+
+    mostrarCantidadDinero();
+}
+
+function mostrarCantidadDinero() {
+    let total = 0;
+    let mensaje = "";
+
+    for(let i = 0; i < cajero.length; i++) {
+        let totalBillete = cajero[i].billetes * cajero[i].cantidad;
+        total += totalBillete;
+        //alert("La cantidad de Billetes de $" + cajero[i].billetes + " es de " + cantidad[i].cantidad + " con un total de $" + totalBillete + ".\n");
+        mensaje += "La cantidad de Billetes de $" + cajero[i].billetes + " es de " + cajero[i].cantidad + " con un total de $" + totalBillete + ".\n";
+    }
+
+    mensaje += "Dando la suma total de: $" + total;
+    alert(mensaje);
+    preguntarEstado();
 
 }
 
+function preguntarEstado() {
+    /**Esta función me dice si el usuario quiere seguir usando el cajero
+     *  o se termina el programa */
+    let estado = confirm("¿Desea realizar otra operación en el cajero?");
+    if (estado) {
+        inicioSesion();
+    } else {
+        alert("Gracias por utilizar nuestros servicios. ¡Hasta pronto!");
+    }
+}
+
 function retirarDinero() {
+    /**Funcion que me permite retirar dinero del cajero
+     * * si el cajero está vacio da un mensaje de mantenimiento
+     */
+    if (cajeroVacio()) {
+        alert('Cajero en mantenimiento, !Vuelva pronto!.');
+        preguntarEstado()
+    }
     
 }
 
 function cajeroVacio() {
+
+    /** Verifica que hay alguna cantidad de dinero */
     for (let i = 0; i < cajero.length; i++) {
-        if (cajero.cantidad > 0) {
+        if (cajero[i].cantidad > 0) {
             return false;
         }
     }
