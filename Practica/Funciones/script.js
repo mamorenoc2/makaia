711561e86e04d1da691889da4f5fd9a0ebd0962d
Manuela Moreno <49201450+mamorenoc2@users.noreply.github.com>
2023-06-22 21:24:08 -0500

    Añadir clasificacion

--- Practica/Funciones/script.js
+++ Practica/Funciones/script.js
@@ -8,6 +8,15 @@ function calcularIMC() {
     
     let imc = peso / (altura * altura);
 
+    let clasificacion 
+    if(imc < 18.5) {
+        clasificacion = "Estas en desnutricion";
+    } else if(imc >= 18.5 && imc < 24.9) {
+        clasificacion = "Estas saludable";
+    } else {
+        clasificacion = "Estas en sobrepeso";
+    }
+
     // Mostrar el resultado en el elemento con el id "resultado"
-    document.getElementById("resultado").textContent = "Hola " + nombre + "! Tu IMC es: " + imc.toFixed(2);
+    document.getElementById("resultado").textContent = "Hola " + nombre + "! Tu IMC es: " + imc.toFixed(2) + "\n " + clasificacion;
 }
